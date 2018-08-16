24d82cc100e7da339970fb2243799c21c9de1c33
Johann Fuechsl <johann@fuechsl.co>
2018-08-16 18:31:56 +0200

    Timeout for the xkcd request

--- simplesvc/xkcd/__init__.py
+++ simplesvc/xkcd/__init__.py
@@ -1,13 +1,22 @@
 import requests
 
-def get_current_xkcd():
-  r = requests.get('https://xkcd.com/info.0.json')
-  if r.status_code != requests.codes.ok:
-    return {
-      'success': False
-    }
-  else:
-    return {
+def _unsuccessful_result():
+  return {
+    'success': False
+  }
+
+def _successful_result(result):
+  return {
       'success': True,
-      'data': r.json()
+      'data': result
     }
+
+def get_current_xkcd():
+  try:
+    r = requests.get('https://xkcd.com/info.0.json', timeout=5)
+    if r.status_code != requests.codes.ok:
+      return _unsuccessful_result()
+    else:
+      return _successful_result(r.json())
+  except:
+    return _unsuccessful_result()
